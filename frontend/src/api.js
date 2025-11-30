3de932ff11b42bdae683938c42fb2145a577d2de
KrishnaveniBojja2005 <bojja0192@gmail.com>
2025-11-30 19:09:18 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+import axios from "axios";
 
-const instance = axios.create({ baseURL: API_BASE });
+const API_BASE = import.meta.env.VITE_API_URL || "https://attendance-backend-adt2.onrender.com/api";
 
+const instance = axios.create({
+  baseURL: API_BASE
+});
+
+// set token automatically
 export const setToken = (token) => {
-  if (token) instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  else delete instance.defaults.headers.common['Authorization'];
+  if (token) {
+    instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete instance.defaults.headers.common["Authorization"];
+  }
 };
 
 export default instance;
